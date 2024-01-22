5f8c56bb6ee7349c98bd6f594396c0e1b7e80b9c
kimtaehoonDev <thkimm.dev@gmail.com>
2024-01-22 19:52:29 +0900

    init

---
+++ src/main/java/com/example/temp240122/Temp240122Application.java
@@ -0,0 +1,13 @@
+package com.example.temp240122;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Temp240122Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Temp240122Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/temp240122/Temp240122ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.temp240122;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Temp240122ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
