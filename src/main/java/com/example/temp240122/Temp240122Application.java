0178bea3c459838b63a0d9f06aa23f770d6076f7
kimtaehoonDev <thkimm.dev@gmail.com>
2024-01-22 20:09:04 +0900

    te

--- src/main/java/com/example/temp240122/Temp240122Application.java
+++ src/main/java/com/example/temp240122/Temp240122Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Temp240122Application {
 
     public static void main(String[] args) {
+        System.out.println("Hello world");
         SpringApplication.run(Temp240122Application.class, args);
     }
 
